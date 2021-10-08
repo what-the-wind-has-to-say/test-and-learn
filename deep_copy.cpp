4991d63f76a2461e015a59fa16b48d7ec3cf1536
miusi <463731786@qq.com>
2021-10-08 16:16:55 +0800

    overload =

--- deep_copy.cpp
+++ deep_copy.cpp
@@ -18,7 +18,23 @@ class Array
             ptr = new int[len];
         }
     }
-    //Array(const Array &arr);
+    Array(const Array &arr);
+    //重载赋值运算符
+    Array &operator=(const Array &arr)
+    {
+        if(this == &arr)
+        {
+            return *this;
+        }
+        if(ptr)
+        {
+            delete []ptr;
+        }
+        ptr = new int(arr.len);
+        memcpy(ptr,arr.ptr,sizeof(int)*arr.len);
+        len = arr.len;
+        return *this;
+    }
     int operator[](int i)const
     {
         return ptr[i];//读取
@@ -27,12 +43,12 @@ class Array
     {
         return ptr[i];//写入
     }
-    int length()
+    int length()const
     {
         return len;
     }
 };
-/*
+
 Array :: Array(const Array &arr)
 {
     if(arr.len == 0)
@@ -48,7 +64,7 @@ Array :: Array(const Array &arr)
         len = arr.len;
     }
 }
-*/
+
 void print_Array(Array &arr)
 {
     for(int i = 0;i < arr.length();i++)
@@ -71,9 +87,13 @@ int main()
         arr[i] = i;
     }
     Array arr2(arr);
+    Array arr3 = arr2;
     arr2[2] = 222;
     arr2[3] = 333;
+    arr3[2] = 233;
+    arr3[3] = 233;
     print_Array(arr);
     print_Array(arr2);
+    print_Array(arr3);
     return 0;
 }
